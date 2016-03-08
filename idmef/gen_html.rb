5391de31bf978e0b3ebeb0b5784ef31cd8267005
Selim Menouar <selim.menouar@rez-gif.supelec.fr>
2016-03-09 00:28:16 +0100

    Correctly handle the base URL

--- idmef/gen_html.rb
+++ idmef/gen_html.rb
@@ -11,6 +11,7 @@ class HTMLGenerator
 
   TITLE = "IDMEF"
   TOPCLASS = "IDMEF-Message"
+  LOCATION = "idmef_parser"
 
   def initialize
     @generator = GraphGenerator.new
@@ -39,15 +40,15 @@ class HTMLGenerator
   end
 
   def render_footer
-    return render FOOTER, {classes: @generator.classes}
+    return render FOOTER, {classes: @generator.classes, location: LOCATION}
   end
 
   def render_head idmef_class
-    return render HEAD, {title: TITLE, classes: @generator.classes, idmef_class: idmef_class}
+    return render HEAD, {title: TITLE, classes: @generator.classes, idmef_class: idmef_class, location: LOCATION}
   end
 
   def render_body idmef_class
-    return render BODY, {idmef_class: @generator.classes[idmef_class], classes: @generator.classes}
+    return render BODY, {idmef_class: @generator.classes[idmef_class], classes: @generator.classes, location: LOCATION}
   end
 
   def render_index idmef_class, name=nil
@@ -56,7 +57,8 @@ class HTMLGenerator
     dataset = {footer: render_footer,
                head: render_head(idmef_class),
                body: render_body(idmef_class),
-               title: TITLE
+               title: TITLE,
+               location: LOCATION
     }
     return render LAYOUT, dataset
   end
